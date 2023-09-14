f539f62877b6eff53eb88b7bec02f7fabc6e75d4
Ujjawal Tanwar <tanwar896@outlook.com>
2023-09-14 23:05:18 +0530

    Update streamlit_app.py
    Fixed bug - hide json data

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ streamlit.write('The user entered ', fruit_choice)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-streamlit.text(fruityvice_response.json()) #Just writes the json data to display
+#streamlit.text(fruityvice_response.json()) #Just writes the json data to display
 
 #take the json version of response & normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
