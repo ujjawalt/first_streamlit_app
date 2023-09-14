186f161bf4fd5a49e2fa4d1292fd927be48c5d44
Ujjawal Tanwar <tanwar896@outlook.com>
2023-09-14 16:37:46 +0530

    Update streamlit_app.py
    Removed the just JSON display output cmd

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) #Just writes the json data to display
+#streamlit.text(fruityvice_response.json()) #Just writes the json data to display
 
 #take the json version of response & normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
