f697fe0eaea2ff5db960ae7b23b36f49f51a1891
Ujjawal Tanwar <tanwar896@outlook.com>
2023-09-16 12:38:15 +0530

    Update streamlit_app.py
    Error fix testing - 3

--- streamlit_app.py
+++ streamlit_app.py
@@ -79,6 +79,6 @@ def insert_row_snowflake(new_fruit):
 add_my_fruit = streamlit.text_input('What fruit would you like add?')
 if streamlit.button('Add a Fruit to the List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    #my_cnx.close()
     back_from_function = insert_row_snowflake(add_my_fruit)
+    my_cnx.close()
     streamlit.text(back_from_function)
