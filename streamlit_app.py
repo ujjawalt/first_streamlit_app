6d6826df48107434d4e3ec94014a1c4f7e289beb
Ujjawal Tanwar <tanwar896@outlook.com>
2023-09-16 12:33:13 +0530

    Update streamlit_app.py
    Error fix testing

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,7 +63,7 @@ def get_fruit_load_list():
 if streamlit.button("Get Fruit List"):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
-    my_cnx.close()
+    #my_cnx.close()
     streamlit.dataframe(my_data_rows)
 
 #Temp fix to keep unwanted getting inserted in snowflake
@@ -79,6 +79,6 @@ def insert_row_snowflake(new_fruit):
 add_my_fruit = streamlit.text_input('What fruit would you like add?')
 if streamlit.button('Add a Fruit to the List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    my_cnx.close()
+    #my_cnx.close()
     back_from_function = insert_row_snowflake(add_my_fruit)
     streamlit.text(back_from_function)
