400bccc5578445856511cf935d17040e5c3bd660
Ujjawal Tanwar <tanwar896@outlook.com>
2023-09-16 11:34:20 +0530

    Update streamlit_app.py
    Error handling functionality added

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,17 +28,23 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
 
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+    if not fruit_choice:
+        streamlit.error("Please select a fruit to get information.")
+    else:
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+        #take the json version of response & normalize it
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        #Output it to the screen as table
+        streamlit.dataframe(fruityvice_normalized)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+except URLError as e:
+    streamlit.error()    
+
+#streamlit.write('The user entered ', fruit_choice)
 #streamlit.text(fruityvice_response.json()) #Just writes the json data to display
-
-#take the json version of response & normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#Output it to the screen as table
-streamlit.dataframe(fruityvice_normalized)
 
 #Temp fix to keep unwanted getting inserted in snowflake
 streamlit.stop()
