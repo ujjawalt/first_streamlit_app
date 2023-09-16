0d28cb769eee641a83d7f0fd26e0ecd8ad004565
Ujjawal Tanwar <tanwar896@outlook.com>
2023-09-16 11:21:13 +0530

    Update streamlit_app.py
    1. Control flow fix to stop records keep getting inserted
    2. Organized code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,20 @@
 import streamlit 
-streamlit.title("My Mom's New Healthy Diner")
+import pandas
+import requests
+import snowflake.connector
+#Import one more lib to audit error
+from urllib.error import URLError
 
-streamlit.header('Breakfast Favorites')
-streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞Avocado Toast')
+streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -25,7 +31,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json()) #Just writes the json data to display
 
@@ -34,7 +40,11 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #Output it to the screen as table
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
+#Temp fix to keep unwanted getting inserted in snowflake
+streamlit.stop()
+
+
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list;")
@@ -48,40 +58,3 @@ streamlit.write('Thanks for adding ', add_my_fruit)
 
 #This will not work
 my_cur.execute("insert into FRUIT_LOAD_LIST values ('from streamlit')")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
